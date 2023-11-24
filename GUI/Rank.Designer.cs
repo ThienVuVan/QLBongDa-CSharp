a75fa3165a170b0e3e96204d6048b098aa8e4bca
TrungDung <dangtrantrungdung16@gmail.com>
2023-11-24 13:03:24 +0700

    dungRankLan2

--- GUI/Rank.Designer.cs
+++ GUI/Rank.Designer.cs
@@ -28,12 +28,357 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.guna2ControlBox3 = new Guna.UI2.WinForms.Guna2ControlBox();
+			this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+			this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.grbDoi = new System.Windows.Forms.GroupBox();
+			this.grbCT = new System.Windows.Forms.GroupBox();
+			this.grbTran = new System.Windows.Forms.GroupBox();
+			this.picLogo = new System.Windows.Forms.PictureBox();
+			this.lbTendoi = new System.Windows.Forms.Label();
+			this.picCT = new System.Windows.Forms.PictureBox();
+			this.lbDiem = new System.Windows.Forms.Label();
+			this.lbTenCT = new System.Windows.Forms.Label();
+			this.lbSobtCT = new System.Windows.Forms.Label();
+			this.lbMaTran = new System.Windows.Forms.Label();
+			this.lbTongBT = new System.Windows.Forms.Label();
+			this.btnDoi = new System.Windows.Forms.Button();
+			this.btnCauthu = new System.Windows.Forms.Button();
+			this.btnTran = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.grbDoi.SuspendLayout();
+			this.grbCT.SuspendLayout();
+			this.grbTran.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picCT)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// guna2ControlBox3
+			// 
+			this.guna2ControlBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.guna2ControlBox3.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+			this.guna2ControlBox3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+			this.guna2ControlBox3.HoverState.Parent = this.guna2ControlBox3;
+			this.guna2ControlBox3.IconColor = System.Drawing.Color.White;
+			this.guna2ControlBox3.Location = new System.Drawing.Point(691, 0);
+			this.guna2ControlBox3.Margin = new System.Windows.Forms.Padding(2);
+			this.guna2ControlBox3.Name = "guna2ControlBox3";
+			this.guna2ControlBox3.ShadowDecoration.Parent = this.guna2ControlBox3;
+			this.guna2ControlBox3.Size = new System.Drawing.Size(34, 24);
+			this.guna2ControlBox3.TabIndex = 1;
+			// 
+			// guna2ControlBox2
+			// 
+			this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
+			this.guna2ControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+			this.guna2ControlBox2.HoverState.Parent = this.guna2ControlBox2;
+			this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+			this.guna2ControlBox2.Location = new System.Drawing.Point(729, 0);
+			this.guna2ControlBox2.Margin = new System.Windows.Forms.Padding(2);
+			this.guna2ControlBox2.Name = "guna2ControlBox2";
+			this.guna2ControlBox2.ShadowDecoration.Parent = this.guna2ControlBox2;
+			this.guna2ControlBox2.Size = new System.Drawing.Size(34, 24);
+			this.guna2ControlBox2.TabIndex = 2;
+			// 
+			// guna2ControlBox1
+			// 
+			this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+			this.guna2ControlBox1.HoverState.Parent = this.guna2ControlBox1;
+			this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+			this.guna2ControlBox1.Location = new System.Drawing.Point(767, 0);
+			this.guna2ControlBox1.Margin = new System.Windows.Forms.Padding(2);
+			this.guna2ControlBox1.Name = "guna2ControlBox1";
+			this.guna2ControlBox1.ShadowDecoration.Parent = this.guna2ControlBox1;
+			this.guna2ControlBox1.Size = new System.Drawing.Size(34, 24);
+			this.guna2ControlBox1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.Control;
+			this.label1.Location = new System.Drawing.Point(299, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(151, 25);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Bảng xếp hạng";
+			// 
+			// grbDoi
+			// 
+			this.grbDoi.BackColor = System.Drawing.Color.PowderBlue;
+			this.grbDoi.Controls.Add(this.label3);
+			this.grbDoi.Controls.Add(this.label2);
+			this.grbDoi.Controls.Add(this.lbDiem);
+			this.grbDoi.Controls.Add(this.lbTendoi);
+			this.grbDoi.Controls.Add(this.picLogo);
+			this.grbDoi.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grbDoi.Location = new System.Drawing.Point(48, 124);
+			this.grbDoi.Name = "grbDoi";
+			this.grbDoi.Size = new System.Drawing.Size(201, 276);
+			this.grbDoi.TabIndex = 5;
+			this.grbDoi.TabStop = false;
+			this.grbDoi.Text = "Đội bóng xuất sắc nhất";
+			// 
+			// grbCT
+			// 
+			this.grbCT.BackColor = System.Drawing.Color.PowderBlue;
+			this.grbCT.Controls.Add(this.label5);
+			this.grbCT.Controls.Add(this.label4);
+			this.grbCT.Controls.Add(this.lbSobtCT);
+			this.grbCT.Controls.Add(this.picCT);
+			this.grbCT.Controls.Add(this.lbTenCT);
+			this.grbCT.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grbCT.Location = new System.Drawing.Point(288, 124);
+			this.grbCT.Name = "grbCT";
+			this.grbCT.Size = new System.Drawing.Size(201, 276);
+			this.grbCT.TabIndex = 5;
+			this.grbCT.TabStop = false;
+			this.grbCT.Text = "Cầu thủ xuất sắc nhất";
+			// 
+			// grbTran
+			// 
+			this.grbTran.BackColor = System.Drawing.Color.PowderBlue;
+			this.grbTran.Controls.Add(this.label7);
+			this.grbTran.Controls.Add(this.label6);
+			this.grbTran.Controls.Add(this.lbTongBT);
+			this.grbTran.Controls.Add(this.lbMaTran);
+			this.grbTran.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.grbTran.Location = new System.Drawing.Point(520, 124);
+			this.grbTran.Name = "grbTran";
+			this.grbTran.Size = new System.Drawing.Size(201, 276);
+			this.grbTran.TabIndex = 5;
+			this.grbTran.TabStop = false;
+			this.grbTran.Text = "Trận đấu";
+			// 
+			// picLogo
+			// 
+			this.picLogo.Location = new System.Drawing.Point(27, 35);
+			this.picLogo.Name = "picLogo";
+			this.picLogo.Size = new System.Drawing.Size(141, 122);
+			this.picLogo.TabIndex = 0;
+			this.picLogo.TabStop = false;
+			// 
+			// lbTendoi
+			// 
+			this.lbTendoi.AutoSize = true;
+			this.lbTendoi.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbTendoi.Location = new System.Drawing.Point(114, 186);
+			this.lbTendoi.Name = "lbTendoi";
+			this.lbTendoi.Size = new System.Drawing.Size(54, 19);
+			this.lbTendoi.TabIndex = 1;
+			this.lbTendoi.Text = "label2";
+			// 
+			// picCT
+			// 
+			this.picCT.Location = new System.Drawing.Point(32, 35);
+			this.picCT.Name = "picCT";
+			this.picCT.Size = new System.Drawing.Size(147, 122);
+			this.picCT.TabIndex = 0;
+			this.picCT.TabStop = false;
+			// 
+			// lbDiem
+			// 
+			this.lbDiem.AutoSize = true;
+			this.lbDiem.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbDiem.Location = new System.Drawing.Point(114, 227);
+			this.lbDiem.Name = "lbDiem";
+			this.lbDiem.Size = new System.Drawing.Size(54, 19);
+			this.lbDiem.TabIndex = 1;
+			this.lbDiem.Text = "label2";
+			// 
+			// lbTenCT
+			// 
+			this.lbTenCT.AutoSize = true;
+			this.lbTenCT.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbTenCT.Location = new System.Drawing.Point(135, 186);
+			this.lbTenCT.Name = "lbTenCT";
+			this.lbTenCT.Size = new System.Drawing.Size(54, 19);
+			this.lbTenCT.TabIndex = 1;
+			this.lbTenCT.Text = "label2";
+			// 
+			// lbSobtCT
+			// 
+			this.lbSobtCT.AutoSize = true;
+			this.lbSobtCT.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbSobtCT.Location = new System.Drawing.Point(135, 227);
+			this.lbSobtCT.Name = "lbSobtCT";
+			this.lbSobtCT.Size = new System.Drawing.Size(54, 19);
+			this.lbSobtCT.TabIndex = 1;
+			this.lbSobtCT.Text = "label2";
+			// 
+			// lbMaTran
+			// 
+			this.lbMaTran.AutoSize = true;
+			this.lbMaTran.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbMaTran.Location = new System.Drawing.Point(137, 67);
+			this.lbMaTran.Name = "lbMaTran";
+			this.lbMaTran.Size = new System.Drawing.Size(54, 19);
+			this.lbMaTran.TabIndex = 1;
+			this.lbMaTran.Text = "label2";
+			// 
+			// lbTongBT
+			// 
+			this.lbTongBT.AutoSize = true;
+			this.lbTongBT.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbTongBT.Location = new System.Drawing.Point(137, 156);
+			this.lbTongBT.Name = "lbTongBT";
+			this.lbTongBT.Size = new System.Drawing.Size(54, 19);
+			this.lbTongBT.TabIndex = 1;
+			this.lbTongBT.Text = "label2";
+			// 
+			// btnDoi
+			// 
+			this.btnDoi.BackColor = System.Drawing.Color.PowderBlue;
+			this.btnDoi.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDoi.Location = new System.Drawing.Point(75, 75);
+			this.btnDoi.Name = "btnDoi";
+			this.btnDoi.Size = new System.Drawing.Size(141, 34);
+			this.btnDoi.TabIndex = 6;
+			this.btnDoi.Text = "BXH Đội bóng";
+			this.btnDoi.UseVisualStyleBackColor = false;
+			// 
+			// btnCauthu
+			// 
+			this.btnCauthu.BackColor = System.Drawing.Color.PowderBlue;
+			this.btnCauthu.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCauthu.Location = new System.Drawing.Point(331, 75);
+			this.btnCauthu.Name = "btnCauthu";
+			this.btnCauthu.Size = new System.Drawing.Size(109, 34);
+			this.btnCauthu.TabIndex = 6;
+			this.btnCauthu.Text = "BXH Cầu thủ";
+			this.btnCauthu.UseVisualStyleBackColor = false;
+			// 
+			// btnTran
+			// 
+			this.btnTran.BackColor = System.Drawing.Color.PowderBlue;
+			this.btnTran.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnTran.Location = new System.Drawing.Point(552, 75);
+			this.btnTran.Name = "btnTran";
+			this.btnTran.Size = new System.Drawing.Size(137, 34);
+			this.btnTran.TabIndex = 6;
+			this.btnTran.Text = "BXH Trận đấu";
+			this.btnTran.UseVisualStyleBackColor = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(23, 186);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(68, 19);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Tên đội: ";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(23, 227);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(51, 19);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Điểm:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(17, 186);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(95, 19);
+			this.label4.TabIndex = 2;
+			this.label4.Text = "Tên cầu thủ:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(17, 227);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(108, 19);
+			this.label5.TabIndex = 3;
+			this.label5.Text = "Số bàn thắng:";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(8, 67);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(103, 19);
+			this.label6.TabIndex = 2;
+			this.label6.Text = "Mã trận đấu: ";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(8, 156);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(123, 19);
+			this.label7.TabIndex = 2;
+			this.label7.Text = "Tổng bàn thắng:";
+			// 
+			// Rank
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::GUI.Properties.Resources._371357042_389339863420825_1783020590677549802_n;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnTran);
+			this.Controls.Add(this.btnCauthu);
+			this.Controls.Add(this.btnDoi);
+			this.Controls.Add(this.grbTran);
+			this.Controls.Add(this.grbCT);
+			this.Controls.Add(this.grbDoi);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.guna2ControlBox3);
+			this.Controls.Add(this.guna2ControlBox2);
+			this.Controls.Add(this.guna2ControlBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Rank";
 			this.Text = "Rank";
+			this.grbDoi.ResumeLayout(false);
+			this.grbDoi.PerformLayout();
+			this.grbCT.ResumeLayout(false);
+			this.grbCT.PerformLayout();
+			this.grbTran.ResumeLayout(false);
+			this.grbTran.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picCT)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox3;
+		private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+		private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+		public System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox grbDoi;
+		private System.Windows.Forms.Label lbTendoi;
+		private System.Windows.Forms.PictureBox picLogo;
+		private System.Windows.Forms.GroupBox grbCT;
+		private System.Windows.Forms.GroupBox grbTran;
+		private System.Windows.Forms.PictureBox picCT;
+		private System.Windows.Forms.Label lbDiem;
+		private System.Windows.Forms.Label lbSobtCT;
+		private System.Windows.Forms.Label lbTenCT;
+		private System.Windows.Forms.Label lbTongBT;
+		private System.Windows.Forms.Label lbMaTran;
+		private System.Windows.Forms.Button btnDoi;
+		private System.Windows.Forms.Button btnCauthu;
+		private System.Windows.Forms.Button btnTran;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
 	}
 }
